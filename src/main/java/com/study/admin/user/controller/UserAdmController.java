8295ec25dcde68d5a46dcd59726e46e69f7f84d4
yong <yong@LAPTOP-OFAHCI28>
2020-10-14 00:41:14 +0900

    테스트

--- src/main/java/com/study/admin/user/controller/UserAdmController.java
+++ src/main/java/com/study/admin/user/controller/UserAdmController.java
@@ -1,5 +1,9 @@
 package com.study.admin.user.controller;
 
+import java.text.BreakIterator;
+import java.util.Locale;
+
+import org.apache.commons.lang3.StringEscapeUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,5 +28,43 @@ public class UserAdmController {
     	
         return "admin/user/loginForm";
     }
+    
+    public static void main(String[] args) {
+    	System.out.println(StringEscapeUtils.escapeJava("🐂 "));
+
+    	final String text = "11한글\ud83d\udc02\ud83d\udc02";
+        
+    	System.out.println(text.substring(2, 3));
+        
+//        final Locale locale = new Locale("hi","IN");
+//        final BreakIterator breaker = BreakIterator.getCharacterInstance(locale);
+        final BreakIterator breaker = BreakIterator.getCharacterInstance();
+        breaker.setText(text);
+        int start = breaker.first();
+        
+        int count = 0;
+        
+        for (int end = breaker.next(); end != BreakIterator.DONE; start = end, end = breaker.next()) {
+
+//        	System.out.println("start:" + start + "end:" + end);
+        	count++;
+        	System.out.println(count);
+        	
+            if (count == 5) {
+            
+            	System.out.println("for : " + text.substring(0, end));
+            	break;
+            }
+        }
+        
+//        BreakIterator it = BreakIterator.getCharacterInstance(); 
+//        it.setText(text); 
+//        int count = 0; 
+//        while (it.next() != BreakIterator.DONE) { 
+//            count++; 
+//        }
+//        
+//        System.out.println(count);
+	}
 
 }
